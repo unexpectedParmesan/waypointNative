d0ade4922ce1ccc1609c0286bf7f7113b892b322
Danielle Knudson <knudson.danielle@gmail.com>
2015-07-10 13:30:21 -0700

    Renamed module from locationTest to Waypoint. Added comments to map view.

--- WaypointTests/WaypointTests.m
+++ WaypointTests/WaypointTests.m
@@ -17,11 +17,11 @@
 #define TIMEOUT_SECONDS 240
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface locationTestTests : XCTestCase
+@interface WaypointTests : XCTestCase
 
 @end
 
-@implementation locationTestTests
+@implementation WaypointTests
 
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
--- index.ios.js
+++ index.ios.js
@@ -14,25 +14,23 @@ var {
 } = React;
 
 // with class definition syntax we extend Component instead of calling createClass
-class locationTest extends React.Component {
+class Waypoint extends React.Component {
 
-  // don't need a function keyword here
+  // Don't need the function keyword when defining functions.
+  // The constructor function is executed when a new instance of the Waypoint is being created
+  // Props is the object passed new Waypoint(), e.g., new Waypoint({ prop1: value, prop2: value })
   constructor(props) {
-    super(props); // you have to call super before you can attach properties
+    // super(props) creates a new instance of the superclass
+    // In this case the superclass is React.Component
+    // super(props) does the same thing that SuperClass.call(this, props) does in pseudoclassical style
+    // super() MUST be called beforing refering to the 'this' of the Waypoint subclass
+    super(props); 
     this.state = {
       position: {
         coords: {}
       }
     };
   } // look ma, no commas!
-
-  getInitialState() {
-    return {
-      annotations: [{
-        title: 'annotation',
-      }]
-    }
-  }
 
   render() {
     return (
@@ -53,7 +51,20 @@ class locationTest extends React.Component {
     );
   }
 
+  // this function will execute after rendering on the client occurs
   componentDidMount() {
+   
+    // navigator is available via the Geolocation polyfill in React Native
+    // http://facebook.github.io/react-native/docs/geolocation.html#content
+    //
+    // navigator is the object through which you interact with the Geolocation interface
+    //
+    // *** Polyfill definition needs to be verified
+    // React Native allows for polyfills--code that provides functionality available in the browser, but
+    // that is not currently available in the runtime environment on mobile devices ***
+    // Geolocation is enabled by default when you create a project with react-native init.
+    //
+    // getCurrentPosition() and watchPostion() take a success callback, error callback, and options object 
     navigator.geolocation.getCurrentPosition(
       (position) => this.setState({position}),
       (error) => alert(error.message),
@@ -82,13 +93,6 @@ var styles = StyleSheet.create({
     alignSelf: 'flex-end',
     marginBottom: 24,
   },
-  // title: {
-  //   fontWeight: 'bold',
-  //   fontSize: 20,
-  //   alignSelf: 'flex-end',
-  //   textAlign: 'center',
-  //   marginTop: 20,
-  // },
   map: {
     position: 'absolute',
     right: 0,
@@ -100,7 +104,6 @@ var styles = StyleSheet.create({
     borderColor: '#dddddd',
     borderRadius: 5,
   },
-
 });
 
-AppRegistry.registerComponent('locationTest', () => locationTest);
+AppRegistry.registerComponent('Waypoint', () => Waypoint);
