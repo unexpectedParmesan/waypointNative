7ed8dabff5ec080233d8c3a08c5b227e67a5a55c
joshturn <talktojosh@gmail.com>
2015-07-11 15:50:37 -0700

    add db model for path

---
+++ Server/db/models/path.js
@@ -0,0 +1,11 @@
+var db = require('./config.js');
+var Waypoint = require('./waypoint.js')
+
+var Path = db.Model.extend({
+	tableName: 'waypoints',
+	waypoint: function(){
+		return this.hasMany(Waypoint);
+	}
+});
+
+module.exports = Path;
