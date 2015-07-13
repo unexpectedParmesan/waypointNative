a9773d34c1c5c0cb1c8b98bbeed96705134df1a4
Colin Parsons <casparsons@gmail.com>
2015-07-13 16:08:51 -0700

    start gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var dbTask = requre('gulp-db')({ user: 'root' });
+var shell = require('gulp-shell');
+
+gulp.task('reset', function() {
+  dbTask.drop('WaypointDb');
+  dbTask.create('WaypointDb');
+  shell.task([
+    'node Server/db/config.js',
+    'node Server/db/populate.js'
+  ]);
+});
