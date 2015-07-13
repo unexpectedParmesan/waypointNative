51bd613eb2d450a25221fe4fde690cc7951398ac
joshturn <talktojosh@gmail.com>
2015-07-13 16:37:41 -0700

    (bug) fix two typos

--- Server/db/config.js
+++ Server/db/config.js
@@ -15,6 +15,7 @@ db.knex.schema.hasTable('paths').then(function(exists) {
 	if (!exists){
 		db.knex.schema.createTable('paths', function(path){
 			path.increments('id').primary();
+			path.integer('creator_id');
 			path.string('title', 100).unique();
 			path.string('length', 100);
 			path.string('description', 5000);
@@ -32,6 +33,7 @@ db.knex.schema.hasTable('waypoints').then(function(exists) {
 		db.knex.schema.createTable('waypoints', function(waypoint){
 			waypoint.increments('id').primary();
 			waypoint.integer('path_id');
+			waypoint.integer('pathIndex');
 			waypoint.float('latitude', 30);
 			waypoint.float('longitude', 30);
 			waypoint.string('title', 100);
