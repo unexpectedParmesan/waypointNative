71e24e2f89f64883d2bf2bef82b1ba0c17e3dd18
Tamara <tmwoodson@gmail.com>
2015-07-13 12:00:34 -0700

    Integrate map with MapQuest API

--- app/Map/map.index.js
+++ app/Map/map.index.js
@@ -1,6 +1,6 @@
 var React = require('react-native');
 var styles = require('./map.styles.js');
-var secret = require('../../secrets.js');
+var secrets = require('../../secrets.js');
 
 var {
   Text,
@@ -46,7 +46,8 @@ class Map extends React.Component {
         number: 3
       },
       distance: 0,
-      current: 0
+      current: 0,
+      directions: {}
     };
     this.state.currentWaypoint = this.state.waypoints.annotations[0];
   } // look ma, no commas!
@@ -60,22 +61,33 @@ class Map extends React.Component {
     return Math.sqrt( Math.pow(latDist, 2) + Math.pow(longDist, 2) );
   }
 
+  _getDirectionsUrl() {
+    var pos1 = this.state.position.coords;
+    var pos2 = this.state.currentWaypoint;
+    var from= 'from=' + pos1.latitude + ',' + pos1.longitude;
+    var to = 'to=' + pos2.latitude + ',' + pos2.longitude;
+    var key = secrets.mapQuest;
+    return 'http://www.mapquestapi.com/directions/v2/route?key=' + key + '&' + from + '&' + to + '&routeType=pedestrian';
+  }
+
   _getDirectionsToNextPoint() {
-    // var pos1 = this.state.position.coords;
-    // var pos2 = this.state.currentWaypoint;
-    // var origin = 'origin=' + pos1.latitude + ',' + pos1.longitude;
-    // var destination = 'destination=' + pos2.latitude + ',' + pos2.longitude;
-    // var key = secret.googleMaps;
-    // var url = 'https://maps.googleapis.com/maps/api/directions/json?' + origin + '&' + destination + '&key=' + key;
-    // console.log('Making google API call: ' + url);
-    // fetch(url)
-    //  .then((response) => response.text())
-    //  .then((responseText) => {
-    //   console.log(responseText);
-    //  })
-    //  .catch((error) => {
-    //   console.warn(error);
-    //  });
+    var url = this._getDirectionsUrl();
+    var context = this;
+    fetch(url)
+     .then((response) => {
+      console.log('response from server: ', response);
+      var mapquestDirections = JSON.parse(response._bodyText);
+      var directions = {};
+      directions.distance = mapquestDirections.route.distance;
+      directions.time = mapquestDirections.route.formattedTime;
+      directions.legs = mapquestDirections.route.legs;
+      context.setState({directions});
+      console.log('Next waypoint: ', context.state.currentWaypoint);
+      console.log('Directions in state: ', context.state.directions);
+     })
+     .catch((error) => {
+      console.warn(error);
+     });
   }
 
   //Add title and current location to map
@@ -100,20 +112,29 @@ class Map extends React.Component {
     );
   }
 
-  _handleArrival() {
-    var currentWaypoint = this.state.waypoints.annotations[this.state.current];
+  _onNextPress()  {
+    console.log('Moving on!');
     var next = this.state.current < this.state.waypoints.number - 1 ? this.state.current + 1 : null;
-    var alertText = next ? 'Next Waypoint' : 'Done!';
-    AlertIOS.alert(
-      currentWaypoint.title,
-      currentWaypoint.subtitle,
-      [
-        {text: alertText, onPress: () => console.log('Moving on!')},
-      ]
-    )
     this.setState({current: next});
     if (next) {
       this.setState({currentWaypoint: this.state.waypoints.annotations[next]});
+      this._getDirectionsToNextPoint();
+    } 
+  }
+
+  _handleArrival() {
+    var context = this;
+    var currentWaypoint = this.state.waypoints.annotations[this.state.current];
+    var next = this.state.current < this.state.waypoints.number - 1 ? this.state.current + 1 : null;
+    var alertText = next ? 'Next Waypoint' : 'Done!';
+    if (currentWaypoint) {
+      AlertIOS.alert(
+        currentWaypoint.title,
+        currentWaypoint.subtitle,
+        [
+          {text: alertText, onPress: context._onNextPress.bind(context)},
+        ]
+      )
     }
   }
 
@@ -131,24 +152,25 @@ class Map extends React.Component {
     // Geolocation is enabled by default when you create a project with react-native init.
     //
     // getCurrentPosition() and watchPostion() take a success callback, error callback, and options object 
+    var context = this;
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.setState({position});
+        context.setState({position});
+        context._getDirectionsToNextPoint();
       },
       (error) => alert(error.message),
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     );
     navigator.geolocation.watchPosition((position) => {
-      this.setState({position});
-      this.setState({distance: this._getDistanceToNextPoint()});
-      if (this.state.distance <= 0.0005) {
-        this._handleArrival();
-      }
+      context.setState({position});
+      context.setState({distance: context._getDistanceToNextPoint()});
+      // if (context.state.distance <= 0.0005) {
+      //   context._handleArrival();
+      // }
     });
-
-    // setTimeout(this._handleArrival.bind(this), 1200);
-    // setTimeout(this._handleArrival.bind(this), 6000);
-    // setTimeout(this._handleArrival.bind(this), 12000);
+    setTimeout(this._handleArrival.bind(this), 6000);
+    setTimeout(this._handleArrival.bind(this), 12000);
+    setTimeout(this._handleArrival.bind(this), 16000);
 
   }
 }
