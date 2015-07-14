4207fc3559cf15714688ee56a518da861c29c4c4
Danielle Knudson <knudson.danielle@gmail.com>
2015-07-13 17:35:42 -0700

    (feat) Add detail view using static data

---
+++ app/Browse/Detail/detail.styles.js
@@ -0,0 +1,72 @@
+var React = require('react-native');
+
+var { StyleSheet } = React;
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    position: 'relative',
+    padding: 20,
+  },
+  content: {
+    position: 'relative',
+    marginTop: 64,
+  },
+  detailsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  title: {
+    fontSize: 25,
+  },
+  creator: {
+    marginTop: 10,
+  },
+  map: {
+    position: 'relative',
+    height: 200,
+    borderWidth: 1,
+    borderColor: '#dddddd',
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  details: {
+    flex: 1,
+    fontSize: 15,
+    lineHeight: 18,
+    color: '#839496',
+    marginTop: 10,
+  },
+  description: {
+    color: '#5b6b77',
+    fontSize: 17,
+    lineHeight: 20,
+    marginTop: 10,
+  },
+  startQuestButton: {
+    marginTop: 20,
+    backgroundColor: '#38A668',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: '#2f8d58',
+    borderRadius: 3,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    // shadowOffset: {
+    //   width: 10,
+    //   height: 10
+    // },
+    // shadowRadius: 1,
+    // shadowColor: '#000000',
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    borderBottomColor: '#000000',
+    borderRightColor: '#000000',
+  },
+});
+
+module.exports = styles;
--- app/Browse/browse.index.js
+++ app/Browse/browse.index.js
@@ -8,6 +8,8 @@ var {
   TouchableHighlight,
   } = React;
 
+var Detail = require('./Detail/detail.index.js');
+
 class Browse extends React.Component {
 
   constructor(props){
@@ -19,9 +21,9 @@ class Browse extends React.Component {
 
   componentDidMount() {
     var paths = [
-        {path: {title: 'Seaside Bike Ride', creator: "DK", description: 'Pleasan bike ride along ocean beach in SF. Pray for sun or else it might not be so pleasant!'}},
-        {path: {title: 'Pub Crawl', creator: "DK", description: 'Hope you\'re thirsty'}},
-        {path: {title: 'SF Tour', creator: "DK", description: 'Tour of San Francisco. Includes audio commentary, pleasant and gross smells, lovely and ugly views, and much, much, more.'}},
+        {title: 'Seaside Bike Ride', numWaypoints: '0', length: '23 miles', estimated_time: '45 minutes', creator: "DK", description: 'Pleasant bike ride along ocean beach in SF. Pray for sun or else it might not be so pleasant!'},
+        {title: 'Pub Crawl', numWaypoints: '5', length: '1.25 miles', estimated_time: '4 hours', creator: "DK", description: 'Hope you\'re thirsty'},
+        {title: 'SF Tour', numWaypoints: '9', length: '8 miles', estimated_time: '3 hours', creator: "DK", description: 'Tour of San Francisco. Includes audio commentary, pleasant and gross smells, lovely and ugly views, and much, much, more.'},
     ];
 
     this.setState({
@@ -39,21 +41,34 @@ class Browse extends React.Component {
   }
 
   renderPath(path) {
+    console.log(path);
     return (
       <TouchableHighlight style={styles.item}
         onPress={this.renderDetailView.bind(this, path)}
         underlayColor={'#FFFFFF'}>
         <View>
-          <Text style={styles.title}>{path.path.title}</Text>
-          <Text style={styles.creator}>Created by {path.path.creator}</Text>
-          <Text style={styles.description}>{path.path.description}</Text>
+          <Text style={styles.title}>{path.title}</Text>
+          <View style={styles.detailsContainer}>
+            <Text style={styles.details}>
+            {path.numWaypoints} stops - {path.length} - {path.estimated_time}</Text>
+          </View>
+          <View>  
+            <Text style={styles.description}>{path.description}</Text>
+          </View>
         </View>
       </TouchableHighlight>
     )
   }
 
   renderDetailView(path) {
-    console.log(path)
+    console.log(path);
+    // "push a new view"
+    this.props.navigator.push({
+      backButtonTitle: ' ',
+      title: path.title,
+      component: Detail,
+      passProps: { details: path }
+    })
   }
 }
 
