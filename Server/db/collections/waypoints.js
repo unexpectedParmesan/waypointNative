6b22813abdcf42b6b49c49575219fdc4d367557c
joshturn <talktojosh@gmail.com>
2015-07-14 17:08:24 -0700

    remove backend files

--- Server/db/collections/waypoints.js
+++
@@ -1,8 +0,0 @@
-var db = require('../config.js');
-var Waypoint = require('../models/waypoint.js');
-
-var Waypoints = new db.Collection();
-
-Waypoints.model = Waypoint;
-
-module.exports = Waypoints;
