8bcf49ff85e462f803b825f010218d3933f300b8
Danielle Knudson <knudson.danielle@gmail.com>
2015-07-14 19:51:07 -0700

    (feat) Start building out Create view

--- app/Create/create.index.js
+++ app/Create/create.index.js
@@ -4,18 +4,61 @@ var styles = require('./create.styles.js')
 var {
   Text,
   View,
+  TextInput,
+  ScrollView,
+  TouchableHighlight,
   } = React;
 
 class Create extends React.Component {
   render() {
-    console.log(this.props.navigator);
-
     return (
-      <View style={styles.container}>
-        <Text style={styles.description}>{this.props.test}</Text>
-      </View>
+      <ScrollView 
+        onScroll={() => { console.log('onScroll!'); }}
+        scrollEventThrottle={200}
+        contentInset={{top: -50}}
+        alwaysBounceVertical={true}
+        style={styles.scroll}>
+        <View style={styles.container}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput 
+            style={styles.textInputShort}
+            autoCapitalize="none"
+            autoCorrect={false} 
+            onChange={this.updateText.bind(this, 'title')}/>
+          <Text style={styles.label}>Quest length (miles)</Text>
+          <TextInput 
+            style={styles.textInputShort}
+            autoCapitalize="none"
+            autoCorrect={false} 
+            onChange={this.updateText.bind(this, 'length')}/>
+          <Text style={styles.label}>Time to complete</Text>
+          <TextInput 
+            style={styles.textInputShort}
+            autoCapitalize="none"
+            autoCorrect={false} 
+            onChange={this.updateText.bind(this, 'estimated_time')} />
+          <Text style={styles.label}>Description</Text>
+          <TextInput 
+            style={styles.textInputLong}
+            autoCapitalize="none" 
+            autoCorrect={false}
+            multiline={true} 
+            onChange={this.updateText.bind(this, 'description')} />
+          <TouchableHighlight 
+            underlayColor={'#2f8d58'}
+            style={styles.addWaypointButton}>
+            <Text style={styles.addWaypointText}>
+              Add waypoint
+            </Text>
+          </TouchableHighlight>
+        </View>
+      </ScrollView>
     )
   } // end of render()
+
+  updateText(field) {
+    console.log(field);
+  }
 } // end of Create class
 
 module.exports = Create;
--- app/Create/create.styles.js
+++ app/Create/create.styles.js
@@ -3,15 +3,45 @@ var React = require('react-native');
 var { StyleSheet } = React;
 
 var styles = StyleSheet.create({
-  description: {
-      fontSize: 20,
-      backgroundColor: 'white'
-  },
   container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-  }
+    flex: 1,
+    padding: 15, 
+    paddingTop: 64,
+  },
+  scroll: {
+    height: 300,
+  },
+  label: {
+    color: '#000000',
+    marginBottom: 5,
+  },
+  textInputShort: {
+    height: 26,
+    borderWidth: 0.5,
+    borderColor: '#C0C0C0',
+    flex: 1,
+    flexDirection: 'row',
+    fontSize: 13,
+    padding: 4,
+    borderRadius: 3,
+    marginBottom: 10,
+  },
+  textInputLong: {
+    height: 70,
+    borderWidth: 0.5,
+    borderColor: '#C0C0C0',
+    flex: 1,
+    fontSize: 13,
+    padding: 4,
+    borderRadius: 3,
+  },
+  addWaypointButton: {
+    marginTop: 15,
+  },
+  addWaypointText: {
+    color: '#196EEE',
+    textAlign: 'right',
+  },
 });
 
 module.exports = styles;
