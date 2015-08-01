95097bbabf0da854ec0f1b575e105882803baebb
Tamara <tmwoodson@gmail.com>
2015-08-01 10:35:09 -0700

    Add missing waypoint icon

--- app/Login/login.index.js
+++ app/Login/login.index.js
@@ -26,7 +26,7 @@ class Login extends React.Component {
     return (
       <View style={styles.loginContainer} >
         <View style={styles.headingContainer}>
-          <Image source={require('image!waypoint_icon_2')} style={styles.icon} />
+          <Image source={require('image!waypoint_icon')} style={styles.icon} />
           <Text style={styles.heading}>Waypoint Beta</Text>
         </View>
         <Image source={require('image!waypoint_map')} style={styles.mapIllustration} />
